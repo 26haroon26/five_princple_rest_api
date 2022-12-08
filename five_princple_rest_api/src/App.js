099e647f2bc4174f2f884e7288d6b4aae69daca3
26haroon26 <26haroon26@gmail.com>
2022-12-08 22:22:40 +0500

    complete

--- five_princple_rest_api/src/App.js
+++ five_princple_rest_api/src/App.js
@@ -1,11 +1,253 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+
+// let baseUrl = "";
+// if (window.location.href.split(":")[0] === "http") {
+//   baseUrl = "http://localhost:4000";
+// } else {
+//   baseUrl = "https://";
+// }
 
 function App() {
+  const [postName, setpostName] = useState("");
+  const [postPrice, setpostPrice] = useState();
+  const [postDescription, setpostDescription] = useState("");
+  const [getData, setgetData] = useState();
+  // const [ProductData, setProductData] = useState();
+  const [istrue, setistrue] = useState(false);
+  const [ProductId, setProductId] = useState("");
+  const [Editing, setEditing] = useState({
+    editingId: null,
+    editingName: "",
+    editingPrice: "",
+    editingDescription: "",
+  });
+  // useEffect(() => {
+    
+  // }, [getData]);
+  const AllProduct = ()=>{
+    axios
+      .get("http://localhost:4000/products")
+      .then((response) => {
+        // console.log(response.data);
+          setgetData(response.data.products);
+          setistrue(!istrue)
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+}
+  const SavePost = () => {
+    axios
+      .post(`http://localhost:4000/product`, {
+        name: postName,
+        price: postPrice,
+        description: postDescription,
+      })
+      .then((response) => {
+        console.log("ok");
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  };
+  const DeletePost = (postId) => {
+    // console.log(postId);
+    axios
+      .delete(`http://localhost:4000/product/${postId}`)
+      .then((response) => {
+        // console.log(response.data);
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  };
+  const Product = (e) => {
+    // console.log(ProductId);
+    e.preventDefault();
+    axios
+      .get(`http://localhost:4000/product/${ProductId}`)
+      .then((response) => {
+        // console.log(response.data.data);
+        // setgetData(response.data.data);
+        setgetData([response.data.data])
+        setistrue(!istrue)
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  };
+  const UpdatePost = (e) => {
+    // console.log(Editing.editingId);
+    axios
+      .put(`http://localhost:4000/product/${Editing.editingId}`, {
+        name: Editing.editingName,
+        price: Editing.editingPrice,
+        description: Editing.editingDescription,
+      })
+      .then((response) => {
+        // console.log(response);
+      });
+    setEditing({
+      editingId: null,
+      editingName: "",
+      editingPrice: "",
+      editingDescription: "",
+    });
+    e.preventDefault();
+  };
   return (
-    <div>
-      app
-    </div>
+    <>
+      <div>
+
+        <form onSubmit={SavePost} className="form">
+          <input
+            className="input"
+            type="text"
+            placeholder="Product name"
+            onChange={(e) => {
+              setpostName(e.target.value);
+              // console.log(e.target.value);
+            }}
+          />
+          <input
+            className="input"
+            type="number"
+            placeholder="price"
+            onChange={(e) => {
+              setpostPrice(e.target.value);
+              // console.log(e.target.value);
+            }}
+          />
+          <input
+            className="input"
+            type="text"
+            placeholder="description"
+            onChange={(e) => {
+              setpostDescription(e.target.value);
+              // console.log(e.target.value);
+            }}
+          />
+          <input type="submit" className="button"value='SetPost' / >
+            
+          
+
+        </form>
+
+        <div className="form newdiv">
+          <form onSubmit={Product}>
+
+          <input
+            required
+            type="number"
+            className="input"
+            onChange={(e) => {
+              setProductId(e.target.value);
+            }}
+            placeholder="Please Enter Id"
+            />
+          <input type="submit" className="button" value='Get 1'/>
+            </form>
+          <button onClick={AllProduct} className="button">Get All</button>
+
+        </div>
+        <div className="body">
+          <div className="flex">
+            {
+            getData?.map((eachPost, i) => {
+              return (
+                <div className="post" key={i}>
+                  <div className="postText">
+                  <p className="overflow">{(eachPost.id === Editing.editingId)? null: `Id :` +eachPost?.id}</p>
+
+                    <h3 className="postDescr"> 
+                      {eachPost.id === Editing.editingId ? (
+                        <form className="NextForm" onSubmit={UpdatePost}>
+                          <input
+                            type="text"
+                            className="input"
+                            defaultValue={Editing.editingName}
+                            onChange={(e) => {
+                              setEditing({
+                                ...Editing,
+                                editingName: e.target.value,
+                              });
+                            }}
+                            placeholder="Please Enter Updated Value"
+                          />
+                          <input
+                            type="number"
+                            className="input"
+                            defaultValue={Editing.editingPrice}
+                            onChange={(e) => {
+                              setEditing({
+                                ...Editing,
+                                editingPrice: e.target.value,
+                              });
+                            }}
+                            placeholder="Price"
+                          />
+                          <input
+                            type="text"
+                            className="input"
+                            defaultValue={Editing.editingDescription}
+                            onChange={(e) => {
+                              setEditing({
+                                ...Editing,
+                                editingDescription: e.target.value,
+                              });
+                            }}
+                            placeholder="description"
+                          />
+                          <input type="submit" className="button next" value='Update'/>
+                        </form>
+                      ) : (
+                        `Name :` + eachPost?.name
+                      )}
+                    </h3>
+                
+                    <span >{(eachPost.id === Editing.editingId)? null: `Price :` + eachPost?.price}</span>
+                    <p className="overflow">{(eachPost.id === Editing.editingId)? null: `Description :` + eachPost?.description}</p>
+                <div style={{margin:'10px auto'}}>
+                    <button className="button"
+                      onClick={() => {
+                        DeletePost(eachPost?.id);
+                      }}
+                    >
+                      Delete
+                    </button>
+                    {Editing.editingId === eachPost?.id ? null : (
+                      <button
+                        className="button"
+                        onClick={() => {
+                          setEditing({
+                            editingId: eachPost?.id,
+                            editingName: eachPost?.name,
+                            editingPrice: eachPost?.price,
+                            editingDescription: eachPost?.description,
+                          });
+                          // console.log(eachPost.id);
+                        }}
+                      >
+                        Edit
+                      </button>
+                    )}
+                  </div> 
+
+                  </div>
+                </div>
+              );
+            })  
+            
+            }
+          </div>
+
+        </div>
+      </div>
+    </>
   );
 }
 
 export default App;
+//
--- server.mjs
+++ server.mjs
@@ -1,85 +1,85 @@
-import express from 'express';
-import path from 'path';
-import cors from 'cors';
+import express from "express";
+import path from "path";
+import cors from "cors";
 
-const app = express()
-const port = process.env.PORT || 5000;
+const app = express();
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
 
 let products = []; // TODO: connect with mongodb instead
 
+app.post("/product", (req, res) => {
+  const body = req.body;
 
-app.post('/product', (req, res) => {
-
-    const body = req.body;
-
-    if ( // validation
-        !body.name
-        && !body.price
-        && !body.description
-    ) {
-        res.status(400).send({
-            message: "required parameters missing",
-        });
-        return;
-    }
-
-    console.log(body.name)
-    console.log(body.price)
-    console.log(body.description)
-
-    products.push({
-        id: new Date().getTime(),
-        name: body.name,
-        price: body.price,
-        description: body.description
+  if (
+    // validation
+    !body.name &&
+    !body.price &&
+    !body.description
+  ) {
+    res.status(400).send({
+      message: "required parameters missing",
     });
-
-    res.send({
-        message: "product added successfully"
-    });
-})
-
-app.get('/products', (req, res) => {
-    res.send({
-        message: "got all products successfully",
-        data: products
-    })
-})
-app.get('/product/:id', (req, res) => {
-
-    const id = req.params.id;
-
-    let isFound = false;
-    for (let i = 0; i < products.length; i++) {
-
-        if (products[i].id === id) {
-            res.send({
-                message: `get product by id: ${products[i].id} success`,
-                data: products[i]
-            });
-
-            isFound = true
-            break;
-        }
-    }
-    if (isFound === false) {
-        res.status(404)
-        res.send({
-            message: "product not found"
-        });
-    }
+    console.log(products);
     return;
-})
+  }
 
-app.delete('/product/:id', (req, res) => {
+  console.log(body.name);
+  console.log(body.price);
+  console.log(body.description);
+
+  products.push({
+    id: new Date().getTime(),
+    name: body.name,
+    price: body.price,
+    description: body.description,
+  });
+
+  res.send({
+    message: "product added successfully",
+  });
+});
+
+app.get("/products", (req, res) => {
+  res.send({
+    message: "got all products successfully",
+    products: products,
+  });
+});
+app.get("/product/:id", (req, res) => {
+  const id = req.params.id;
+console.log(id);
+  let isFound = false;
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].id == id) {
+      res.send({
+        message: `get product by id: ${products[i].id} success`,
+        data: products[i],
+      });
+
+      isFound = true;
+      break;
+    }
+  }
+  if (isFound === false) {
+    res.status(404);
+    res.send({
+      message: "product not found",
+    });
+  }
+  return;
+});
+
+app.delete("/product/:id", (req, res) => {
     const id = req.params.id;
+    // console.log( 'get id',id);
 
     let isFound = false;
     for (let i = 0; i < products.length; i++) {
-        if (products[i].id === id) {
+        if (products[i].id == id) {
+            // console.log('products[i].id' ,products[i].id);
             products.splice(i, 1);
             res.send({
                 message: "product deleted successfully"
@@ -93,62 +93,60 @@ app.delete('/product/:id', (req, res) => {
         res.send({
             message: "delete fail: product not found"
         });
-    }
-})
+    } 
 
-app.put('/product/:id', (req, res) => {
+});
+app.put("/product/:id", (req, res) => {
+  const body = req.body;
 
-    const body = req.body;
-    const id = req.params.id;
+  console.log(body);
+  const id = req.params.id;
 
-    if ( // validation
-        !body.name
-        && !body.price
-        && !body.description
-    ) {
-        res.status(400).send({
-            message: "required parameters missing"
-        });
-        return;
-    }
-
-    console.log(body.name)
-    console.log(body.price)
-    console.log(body.description)
-
-    let isFound = false;
-    for (let i = 0; i < products.length; i++) {
-        if (products[i].id === id) {
-
-            products[i].name = body.name;
-            products[i].price = body.price;
-            products[i].description = body.description;
-
-            res.send({
-                message: "product modified successfully"
-            });
-            isFound = true
-            break;
-        }
-    }
-    if (!isFound) {
-        res.status(404)
-        res.send({
-            message: "edit fail: product not found"
-        });
-    }
-    res.send({
-        message: "product added successfully"
+  if (
+    // validation
+    !body.name &&
+    !body.price &&
+    !body.description
+  ) {
+    res.status(400).send({
+      message: "required parameters missing",
     });
-})
+    return;
+  }
 
+  console.log(body.name);
+  console.log(body.price);
+  console.log(body.description);
 
+  let isFound = false;
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].id == id) {
+      products[i].name = body.name;
+      products[i].price = body.price;
+      products[i].description = body.description;
+
+      res.send({
+        message: "product modified successfully",
+      });
+      isFound = true;
+      break;
+    }
+  }
+  if (!isFound) {
+    // res.status(404);
+    res.send({
+      message: "edit fail: product not found",
+    });
+  }
+  // res.send({
+  //   message: "product added successfully",
+  // });
+});
 
 const __dirname = path.resolve();
-app.use('/', express.static(path.join(__dirname, './web/build')))
-app.use('*', express.static(path.join(__dirname, './web/build')))
-
+app.use("/", express.static(path.join(__dirname, "./five_princple_rest_api/build")));
+app.use("*", express.static(path.join(__dirname, "./five_princple_rest_api/build")));
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
